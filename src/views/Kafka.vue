bd0603b0ea445a6e6ff200bba145d9df9fdeeca7
Dininta Isnurthina <dini.dininta@gmail.com>
2020-07-07 23:02:11 +0700

    Add key bind in <p> v-for component

---
+++ src/views/Kafka.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about">
+    <button @click="submitOrder">Order SLIK</button>
+    <p v-for="(key, value) in Object.entries(customer)" :key="key">{{key}} : {{value}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Kafka',
+  data() {
+    return {
+      customer: {},
+    };
+  },
+  methods: {
+    submitOrder() {
+      alert('order submitted');
+    },
+  },
+};
+</script>
